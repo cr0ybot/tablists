d406548589af1e8388c5c8da05f66a8ea5ab6221
Cory Hughart <cory@blackbirdi.com>
2015-12-03 22:30:13 -0500

    added grunt
    Node & Grunt in preparation for SASS

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dev: {
+                options:{
+                    outputStyle:'expanded',
+                    sourceMap: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'scss',
+                    src: ['**/*.scss'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
+            },
+            dist: {
+                options: {
+                    outputStyle:'compressed',
+                    sourceMap: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'scss',
+                    src: ['**/*.scss'],
+                    dest: 'css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        postcss: {
+            options: {
+                remove: false,
+                processors: [
+                    require('autoprefixer')({
+                        browsers: ['> 1%', 'last 2 versions']
+                    })
+                ]
+            },
+            dev: {
+                options: {
+                    map: true
+                },
+                src: 'css/*.css'
+            },
+            dist: {
+                options: {
+                    map: false
+                },
+                src: 'css/*.css'
+            }
+        },
+
+        uglify: {
+            options: {
+                files: [{
+                    expand: true,
+                    cwd: '.',
+                    src: ['**/*[^min].js'],
+                    dest: '.',
+                    ext: '.min.js'
+                }]
+            },
+            dev: {
+                sourceMap: true
+            },
+            dist: {
+                sourceMap: false
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true
+            },
+            html: {
+                files: '**/*.html'
+            },
+            css: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass:dev', 'postcss:dev']
+            },
+            js: {
+                files: '**/*.js',
+                tasks: ['uglify:dev']
+            }
+        }
+    });
+
+    grunt.registerTask('default',['sass:dev','postcss:dev','watch']);
+    grunt.registerTask('build',['sass:dist','postcss:dist','uglify:dist']);
+};
