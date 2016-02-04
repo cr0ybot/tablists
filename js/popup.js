27b8e6cec899bf4ab247a0cd9555eac5e473fcac
Cory Hughart <cory@blackbirdi.com>
2016-02-03 22:18:58 -0500

    popup styles and boilerplate js

--- js/popup.js
+++ js/popup.js
@@ -3,3 +3,28 @@
  * A Firefox TabGroups/Panorama Replacement
  * by Cory Hughart <cory@coryhughart.com>
  */
+
+function TabListPopup() {
+    console.log('TabListPopup initialized!');
+
+    var me = this;
+
+    // Initialize via message to background
+    chrome.runtime.sendMessage({request: 'tabLists'}, me.init);
+
+    this.init = function(baseTabLists) {
+        tabLists = baseTabLists;
+
+        console.log('TabListPopup loaded');
+
+        document.body.removeChild(loading);
+    }
+} // end TabListPopup
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var loading = document.getElementById('loading');
+
+    var tabLists;
+
+    var tlp = new TabListPopup();
+});
