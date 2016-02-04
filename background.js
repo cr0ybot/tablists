e1c0b7e990ef812ff18b27273c24358a577eb17d
Cory Hughart <cory@blackbirdi.com>
2016-02-03 22:20:02 -0500

    tab sync on startup

--- background.js
+++ background.js
@@ -13,66 +13,130 @@ var defaultOptions = {
     },
     options,
     bookmarkTree,
-    curentTabs;
+    curentTabs,
+    tabLists;
 
+/**
+ * TabListManager class
+ * Since there will be only one, we are not using the "prototype" pattern,
+ * especially since we need to be able to reference "this" quite often.
+ * @constructor
+ * @param {object} options - options object
+ * @param {string} options.folderID - id of the folder that TabLists bookmarks will be saved to
+ * @param {string} options.folderName - name of the folder that TabLists bookmarks will be saved to
+ * @param {number} options.activeList - the id of the TabList that is currently active
+ * @param {array} options.activeTab - an array of the active tab id for each TabList
+ * @param {number} options.startupList - the id of the TabList to load at startup, or null for the last list that was active (overrides activeList)
+ */
 function TabListManager(passedOptions) {
-    console.log('TabLists initialized!');
+    console.log('TabListManager initialized!');
 
     var me = this;
 
-    // Global message listener
-    chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+    // TabListManager event listeners
 
-        // Submit
-        if (message.submit) {
-            if (message.data !== undefined) {
-                console.log('Submit message from '+sender+' received: '+JSON.stringify(message.data));
-                switch (message.submit) {
+    this.initMessageListener = function() {
+
+        chrome.extension.onMessage.addListener(function(message, sender, callback) {
+
+            // Submit
+            if (message.submit) {
+                if (message.data !== undefined) {
+                    console.log('Submit message from '+sender+' received: '+JSON.stringify(message.data));
+                    switch (message.submit) {
+                        case 'options':
+                            me.saveOptions(message.data);
+                            callback(message.data);
+                        break;
+                        default:
+                            console.warn('Submit message from '+sender+' reveived but not understood: '+JSON.stringify(message.data));
+                        break;
+                    }
+                }
+                else {
+                    console.warn('Submit message from '+sender+' received but no data was transmitted');
+                }
+            }
+
+            // Request
+            else if (message.request) {
+                console.log('Request message from '+sender+' received: '+message.request);
+
+                switch (message.request) {
                     case 'options':
-                        me.saveOptions(message.data);
-                        me.sendResponse(message.data);
+                        callback(options);
+                    break;
+                    case 'defaultOptions':
+                        callback(defaultOptions);
+                    break;
+                    case 'tabLists':
+                        callback(tabLists);
                     break;
                     default:
-                        console.warn('Submit message from '+sender+' reveived but not understood: '+JSON.stringify(message.data));
+                        console.warn('Request message from '+sender+' received but not understood: '+message.request);
                     break;
                 }
             }
-            else {
-                console.warn('Submit message from '+sender+' received but no data was transmitted');
-            }
-        }
-
-        // Request
-        else if (message.request) {
-            console.log('Request message from '+sender+' received: '+message.request);
-
-            switch (message.request) {
-                case 'options':
-                    me.sendResponse(options);
-                break;
-                case 'defaultOptions':
-                    me.sendResponse(defaultOptions);
-                break;
-                default:
-                    console.warn('Request message from '+sender+' received but not understood: '+message.request);
-                break;
-            }
-        }
-    });
-
-    if (typeof passedOptions === 'undefined') {
-        // init settings using chrome.storage.local (check if exists first)
-        console.log('Getting options from storage...');
-        chrome.storage.local.get(function(items) {
-            me.checkOptions(items);
         });
     }
-    else {
-        console.log('Options passed to init: '+JSON.stringify(passedOptions));
-        me.checkOptions(passedOptions);
+
+    this.initTabListeners = function() {
+        console.log('Initializing tab event listeners...');
+        /*
+        onCreated
+        onUpdated
+        onMoved
+        onActivated
+        onHighlighted
+        onDetached
+        onAttached
+        onRemoved
+        onReplaced
+        onZoomChange
+        */
+        chrome.tabs.onCreated.addListener(this.tabCreatedHandler);
+        chrome.tabs.onUpdated.addListener(this.tabUpdatedHandler);
+        chrome.tabs.onMoved.addListener(this.tabMovedHandler);
+        chrome.tabs.onActivated.addListener(this.tabActivatedHandler);
+        chrome.tabs.onDetached.addListener(this.tabDetachedHandler);
+        chrome.tabs.onAttached.addListener(this.tabAttachedHandler);
+        chrome.tabs.onRemoved.addListener(this.tabRemovedHandler);
+        chrome.tabs.onReplaced.addListener(this.tabReplacedHandler);
     }
 
-    // TabListManager methods
+    this.tabCreatedHandler = function(tab) {
+        // TODO
+    }
+
+    this.tabUpdatedHandler = function(tabId, changeInfo, tab) {
+        // TODO
+    }
+
+    this.tabMovedHandler = function(tabId, moveInfo) {
+        // TODO
+    }
+
+    this.tabActivatedHandler = function(activeInfo) {
+        // TODO
+    }
+
+    this.tabDetachedHandler = function(tabId, detachInfo) {
+        // TODO
+    }
+
+    this.tabAttachedHandler = function(tabId, attachInfo) {
+        // TODO
+    }
+
+    this.tabRemovedHandler = function(tabId, removeInfo) {
+        // TODO
+    }
+
+    this.tabReplacedHandler = function(addedTabId, removedTabId) {
+        // TODO
+    }
+
+    // TabListManager class methods
 
     this.checkOptions = function(passedOptions) {
         console.log('Checking options...');
@@ -90,7 +154,7 @@ function TabListManager(passedOptions) {
         if (typeof options.folderID !== 'string' && isNaN(options.folderID)) {
             console.warn('TabLists folderID not found');
             me.searchForBookmarksFolder(function(o) {
-                me.saveOptions(o, checkOptions);
+                me.saveOptions(o, me.checkOptions);
             });
             // We need to end early because checking for and creating the bookmarks folder is asynchronous
             return;
@@ -181,7 +245,7 @@ function TabListManager(passedOptions) {
                 console.warn(chrome.runtime.lastError.message);
                 // Wrong folderID, we'll assume the folder no longer exists
                 me.createBookmarksFolder(function(o) {
-                    saveOptions(o, checkOptions);
+                    me.saveOptions(o, me.checkOptions);
                 });
             }
 
@@ -194,7 +258,7 @@ function TabListManager(passedOptions) {
             }
             else {
                 me.createBookmarksFolder(function(o) {
-                    me.saveOptions(o, checkOptions);
+                    me.saveOptions(o, me.checkOptions);
                 });
             }
         });
@@ -228,14 +292,30 @@ function TabListManager(passedOptions) {
             console.log(currentTabs);
             console.groupEnd();
 
-            for (i = 0; i < currentTabs.length; i++) {
-                if (currentTabs[i].url != list[i].url) {
-                    console.log('Your tabs are not synced with the list!');
-                    if (confirm('Your tabs are not synced with TabList '+listID+'!\n\nSync now?')) {
-                        // sync tabs to list
-                    }
-                    break;
+            if (list.length != currentTabs.length) {
+                console.log('Your tabs are not synced with the list!');
+                if (confirm('Your tabs are not synced with TabList '+listID+'!\n\nSync now?')) {
+                    // sync tabs to list
+                    me.syncTabsToList(listID, theTabs, callback);
                 }
+            }
+            else if (list.length > 0) {
+                for (i = 0; i < currentTabs.length; i++) {
+                    if (i >= list.length || currentTabs[i].url != list[i].url) {
+                        console.log('Your tabs are not synced with the list!');
+                        if (confirm('Your tabs are not synced with TabList '+listID+'!\n\nSync now?')) {
+                            // sync tabs to list
+                            me.syncTabsToList(listID, theTabs, callback);
+                        }
+                        break;
+                    }
+                    else {
+                        console.log('Bookmark '+i+' is in sync');
+                    }
+                }
+            }
+            else {
+                me.syncTabsToList(listID, theTabs, callback);
             }
         }
 
@@ -256,6 +336,44 @@ function TabListManager(passedOptions) {
         }
     } // end compareListToTabs
 
+    this.syncTabsToList = function(listID, tabs, callback) {
+        console.group('Syncing tabs to list '+listID);
+
+        var list = bookmarkTree.children[listID].children;
+
+        for (var i = 0; i < tabs.length; i++) {
+            // check if index fits in current bookmark list
+            // if it does, we can update the bookmark instead of deleting & creating one
+            if (i < list.length) {
+                console.log('Updating bookmark '+i+' ('+list[i].id+')');
+
+                chrome.bookmarks.update(list[i].id, {
+                    'title': tabs[i].title,
+                    'url': tabs[i].url
+                });
+
+                // if we're within the original bookmark list length but this is the last tab,
+                // the rest of the bookmarks need to be deleted
+                if (i == tabs.length - 1) {
+                    for (var j = i + 1; j < list.length; j++) {
+                        console.log('Removing bookmark '+j+' ('+list[j].id+')');
+                        chrome.bookmarks.remove(list[j].id);
+                    }
+                }
+            }
+            // if we're past the original length, we need to create new bookmarks
+            else {
+                console.log('Creating bookmark '+i);
+                chrome.bookmarks.create({
+                    'parentId': bookmarkTree.children[listID].id,
+                    'title': tabs[i].title,
+                    'url': tabs[i].url
+                });
+            }
+            console.groupEnd();
+        }
+    } // end syncTabsToList
+
     this.createList = function(title, callback) {
         chrome.bookmarks.create({
             parentId: options.folderID,
@@ -268,6 +386,40 @@ function TabListManager(passedOptions) {
             if (typeof callback === 'function') callback(results);
         });
     } // end createList
-} // end TabListManager
 
+    // TabListManager constructor
+
+    // Global message listener
+    me.initMessageListener();
+
+    // Tab listeners
+    me.initTabListeners();
+
+    // Options initialization
+    if (typeof passedOptions === 'undefined') {
+        // init settings using chrome.storage.local (check if exists first)
+        console.log('Getting options from storage...');
+        chrome.storage.local.get(function(items) {
+            me.checkOptions(items);
+        });
+    }
+    else {
+        console.log('Options passed to init: '+JSON.stringify(passedOptions));
+        me.checkOptions(passedOptions);
+    }
+}
+// end TabListManager class
+
+
+// TabList class
+function TabList(bookmarkTree) {
+    console.log('TabList initialized!');
+
+    var me = this;
+
+
+} // end TabList class
+
+
+// Initialize TabListManager
 var tlm = new TabListManager();
